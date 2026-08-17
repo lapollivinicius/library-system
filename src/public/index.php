2fdbc1c606764ebd3ac68bd246eee6b9be3c5028
vini <vinidemoraes05@gmail.com>
2026-08-17 11:28:59 -0300

    chore: add init session in index.php

--- src/public/index.php
+++ src/public/index.php
@@ -2,11 +2,13 @@
 
 require_once '../config/middleware.php';
 require_once '../config/database.php';
+require_once '../config/session.php';
 require_once '../config/routes.php';
 
 try {
   $db = config\database::connect();
   $setup = config\database::setup($db);
+  $session = config\session::init();
   $routes = new config\routes();
 } catch (\Throwable $error) {
   echo $error->getMessage();
