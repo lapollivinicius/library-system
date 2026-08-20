5a993bd79a490bb47d854e91b92cfde2897ff476
vini <vinidemoraes05@gmail.com>
2026-08-19 23:22:03 -0300

    chore: change sign

--- src/public/js/sign.js
+++ src/public/js/sign.js
@@ -1,2 +1,14 @@
-console.log("created by lapollivinicius");
-console.log("https://github.com/lapollivinicius");
+console.log(
+  `\n%c███████\n%c██%c███%c██\n%c███████\n%c██%c███%c██\n%c███████\n`,
+  'color: white; font-weight: bold;',
+  'color: black; font-weight: bold;',
+  'color: white; font-weight: bold;',
+  'color: black; font-weight: bold;',
+  'color: #55aa55; font-weight: bold;',
+  'color: white; font-weight: bold;',
+  'color: red; font-weight: bold;',
+  'color: white; font-weight: bold',
+  'color: white; font-weight: bold',
+  `\ncreated by lapolli
+▸ https://github.com/lapollivinicius
+`)
