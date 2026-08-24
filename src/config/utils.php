6e3be5db7ec5eebfa2d0e4c6c37117fce295c102
vini <229406217+lapollivinicius@users.noreply.github.com>
2026-08-24 17:21:16 -0300

    chore: add code hex generator with prefix

--- src/config/utils.php
+++ src/config/utils.php
@@ -12,4 +12,10 @@ class utils
     $data[8] = chr((ord($data[8]) & 0x3f) | 0x80);
     return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
   }
+
+  public static function code(string $prefix): string
+  {
+    $hex = strtoupper(bin2hex(random_bytes(3)));
+    return strtoupper($prefix) . '-' . $hex;
+  }
 }
