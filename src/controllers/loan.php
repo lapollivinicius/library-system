ad03702b5d36573e6e44f3db048767b62ff7bcb8
vini <vinidemoraes05@gmail.com>
2026-08-23 22:48:06 -0300

    chore: init controllers to members and loans

--- src/controllers/loan.php
+++ src/controllers/loan.php
@@ -2,11 +2,36 @@
 
 namespace controllers;
 
+require_once '../models/loan.php';
+
 class loan extends controller
 {
 
   public function index()
   {
     $this->render('app/loans');
+  }
+
+  public function editLoan(array $params = [])
+  {
+    $id = $params['id'] ?? null;
+
+    if (!$id) {
+      header('location: /loans');
+      exit;
+    }
+
+    $database = \config\database::connect();
+    $model    = new \models\loan($database);
+
+    $loan = $model->find($id);
+
+    if (!$loan) {
+      header('location: /loans');
+      exit;
+    }
+
+    $this->view->loan = $loan;
+    $this->render('app/edit-loan');
   }
 }
--- src/controllers/member.php
+++ src/controllers/member.php
@@ -2,11 +2,36 @@
 
 namespace controllers;
 
+require_once '../models/member.php';
+
 class member extends controller
 {
 
   public function index()
   {
     $this->render('app/members');
+  }
+
+  public function editMember(array $params = [])
+  {
+    $id = $params['id'] ?? null;
+
+    if (!$id) {
+      header('location: /members');
+      exit;
+    }
+
+    $database = \config\database::connect();
+    $model    = new \models\member($database);
+
+    $member = $model->find($id);
+
+    if (!$member) {
+      header('location: /members');
+      exit;
+    }
+
+    $this->view->member = $member;
+    $this->render('app/edit-member');
   }
 }
