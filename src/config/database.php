15dad266557e1e741646a16eeb79d35994a35803
vini <229406217+lapollivinicius@users.noreply.github.com>
2026-08-27 17:52:46 -0300

    chore: change indexes in database -> members and books

--- src/config/database.php
+++ src/config/database.php
@@ -20,56 +20,61 @@ class database
   {
     return $database->exec('
           CREATE TABLE IF NOT EXISTS users (
-              user_id VARCHAR(36) PRIMARY KEY NOT NULL,
-              name VARCHAR(64) NOT NULL,
-              email VARCHAR(64) UNIQUE NOT NULL,
-              password VARCHAR(255) NOT NULL,
-              library_name VARCHAR(64) NOT NULL,
-              is_active BOOLEAN NOT NULL DEFAULT TRUE
+            user_id VARCHAR(36) PRIMARY KEY NOT NULL,
+            name VARCHAR(64) NOT NULL,
+            email VARCHAR(64) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            library_name VARCHAR(64) NOT NULL,
+            is_active BOOLEAN NOT NULL DEFAULT TRUE
           );
           CREATE TABLE IF NOT EXISTS members (
-              member_id VARCHAR(36) PRIMARY KEY NOT NULL,
-              user_id VARCHAR(36) NOT NULL,
-              code INT AUTO_INCREMENT UNIQUE,
-              name VARCHAR(64) NOT NULL,
-              email VARCHAR(64) UNIQUE NOT NULL,
-              phone VARCHAR(64) UNIQUE NOT NULL,
-              is_active BOOLEAN NOT NULL DEFAULT TRUE,
+            member_id VARCHAR(36) PRIMARY KEY NOT NULL,
+            user_id VARCHAR(36) NOT NULL,
+            code VARCHAR(16) NOT NULL,
+            name VARCHAR(64) NOT NULL,
+            email VARCHAR(64) NOT NULL,
+            phone VARCHAR(36) NOT NULL,
+            document VARCHAR(36) NOT NULL,
+            is_active BOOLEAN NOT NULL DEFAULT TRUE,
 
-              FOREIGN KEY (user_id) REFERENCES users(user_id)
+            UNIQUE KEY unique_user_code (user_id, code),
+            UNIQUE KEY unique_user_name (user_id, name),
+            UNIQUE KEY unique_user_email (user_id, email),
+
+            FOREIGN KEY (user_id) REFERENCES users(user_id)
           );
           CREATE TABLE IF NOT EXISTS books (
-              book_id VARCHAR(36) PRIMARY KEY NOT NULL,
-              user_id VARCHAR(36) NOT NULL,
-              code VARCHAR(16) NOT NULL,
-              title VARCHAR(64) NOT NULL,
-              author VARCHAR(64) NOT NULL,
-              isbn VARCHAR(64) NOT NULL,
-              genre VARCHAR(64) NOT NULL,
-              year VARCHAR(4) NOT NULL,
-              quantity INT NOT NULL DEFAULT 0,
-              is_active BOOLEAN NOT NULL DEFAULT TRUE,
+            book_id VARCHAR(36) PRIMARY KEY NOT NULL,
+            user_id VARCHAR(36) NOT NULL,
+            code VARCHAR(16) NOT NULL,
+            title VARCHAR(64) NOT NULL,
+            author VARCHAR(64) NOT NULL,
+            isbn VARCHAR(64) NOT NULL,
+            genre VARCHAR(64) NOT NULL,
+            year VARCHAR(4) NOT NULL,
+            quantity INT NOT NULL DEFAULT 0,
+            is_active BOOLEAN NOT NULL DEFAULT TRUE,
 
-              FOREIGN KEY (user_id) REFERENCES users(user_id),
+            FOREIGN KEY (user_id) REFERENCES users(user_id),
 
-              UNIQUE KEY unique_user_title (user_id, title),
-              UNIQUE KEY unique_user_code (user_id, code)
+            UNIQUE KEY unique_user_title (user_id, title),
+            UNIQUE KEY unique_user_code (user_id, code)
           );
           CREATE TABLE IF NOT EXISTS loans (
-              loan_id VARCHAR(36) PRIMARY KEY NOT NULL,
-              user_id VARCHAR(36) NOT NULL,
-              member_id VARCHAR(36) NOT NULL,
-              
-              book_id VARCHAR(36) NOT NULL,
-              code INT AUTO_INCREMENT UNIQUE,
+            loan_id VARCHAR(36) PRIMARY KEY NOT NULL,
+            user_id VARCHAR(36) NOT NULL,
+            member_id VARCHAR(36) NOT NULL,
+            
+            book_id VARCHAR(36) NOT NULL,
+            code VARCHAR(16) NOT NULL,
 
-              loaned_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-              due_at DATETIME NOT NULL,
-              returned_at DATETIME NULL,
+            loaned_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            due_at DATETIME NOT NULL,
+            returned_at DATETIME NULL,
 
-              FOREIGN KEY (user_id) REFERENCES users(user_id),
-              FOREIGN KEY (member_id) REFERENCES members(member_id),
-              FOREIGN KEY (book_id) REFERENCES books(book_id)
+            FOREIGN KEY (user_id) REFERENCES users(user_id),
+            FOREIGN KEY (member_id) REFERENCES members(member_id),
+            FOREIGN KEY (book_id) REFERENCES books(book_id)
           );
       ') !== false;
   }
