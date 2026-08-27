d16e5137c23f7dc534efe44bd7a38bb65a5e5f27
vini <229406217+lapollivinicius@users.noreply.github.com>
2026-08-27 13:46:13 -0300

    fix: change session and user_id config - getting if isnt empty

--- src/controllers/controller.php
+++ src/controllers/controller.php
@@ -14,13 +14,11 @@ abstract class controller
   public function __construct()
   {
     $this->view = new \stdClass();
-
-    if (empty($_SESSION['user_id'])) {
-      header('Location: /login');
-      exit;
+    
+    if(!empty($_SESSION['user_id'])) {
+      $this->user_id = $_SESSION['user_id'];
     }
 
-    $this->user_id = $_SESSION['user_id'];
   }
 
   protected function render(
